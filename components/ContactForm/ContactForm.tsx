dd9ed6f6d4b9e7f7f5507e2c895f6af0134819d5
Andantez <kaloyanatanasov83@gmail.com>
2023-01-13 20:40:46 +0000

    feat: add html and add the react hook.

--- components/ContactForm/ContactForm.tsx
+++ components/ContactForm/ContactForm.tsx
@@ -1,7 +1,44 @@
+import { useForm, SubmitHandler } from 'react-hook-form';
+
+type FormInputs = {
+  name: string;
+  email: string;
+  subject: string;
+  message: string;
+};
 const ContactForm = (): JSX.Element => {
-  return <form action="">
-    
-  </form>;
+  const { register, handleSubmit } = useForm<FormInputs>();
+
+  const onSubmit: SubmitHandler<FormInputs> = (data) => {
+    console.log(data);
+    // TODO: to be implemented
+  };
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} style={{ display: 'grid' }}>
+      <div>
+        <label htmlFor="name">Name</label>
+        <input type="text" id="name" {...register('name')} />
+      </div>
+      <div>
+        <label htmlFor="email">Email</label>
+        <input type="email" id="email" {...register('email')} />
+      </div>
+      <div>
+        <label htmlFor="subject">Subject</label>
+        <input type="text" id="subject" {...register('subject')} />
+      </div>
+      <div>
+        <label htmlFor="message">Message</label>
+        <textarea
+          id="message"
+          cols={30}
+          rows={10}
+          {...register('message')}
+        ></textarea>
+      </div>
+      <button type="submit">Send your message</button>
+    </form>
+  );
 };
 
 export default ContactForm;
