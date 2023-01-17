33b01e94064e07d44936166b506ab3c61d3bb74e
Andantez <kaloyanatanasov83@gmail.com>
2023-01-17 22:40:18 +0000

    fix: message validation error.

--- lib/helpers/index.ts
+++ lib/helpers/index.ts
@@ -1,5 +1,5 @@
 import { MAX_MESSAGE_LENGTH } from '../constVariables';
 
 export const charsLeft = (v: string) =>
-  MAX_MESSAGE_LENGTH - parseInt(v) < MAX_MESSAGE_LENGTH ||
+  MAX_MESSAGE_LENGTH - v.length < MAX_MESSAGE_LENGTH ||
   `${v.length} out of ${MAX_MESSAGE_LENGTH} characters left`;
