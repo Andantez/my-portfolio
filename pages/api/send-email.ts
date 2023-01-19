5659c0fdea2b0b6865f1b457935f599b26c42d9e
Kaloyan Atanasov <kaloyanatanasov83@gmail.com>
2023-01-19 23:23:57 +0000

    change: remove the switch statement.

--- pages/api/send-email.ts
+++ pages/api/send-email.ts
@@ -1,18 +1,13 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  switch (req.method) {
-    case 'POST': {
-      res
-        .status(201)
-        .json({
-          status: 'success',
-          message: 'Thank You. I will get back at you as soon as possible!',
-        });
-      break;
-    }
-    default:
-      res.setHeader('Allow', ['POST']);
-      res.status(405).end(`Method ${req.method} Not Allowed`);
+  if (req.method !== 'POST') {
+    res.setHeader('Allow', ['POST']);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
   }
+
+  return res.status(201).json({
+    status: 'success',
+    message: 'Thank You. I will get back at you as soon as possible!',
+  });
 }
