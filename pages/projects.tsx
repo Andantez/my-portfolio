be6ffb708e32ff7a3845343340c798809e0f51d6
Kaloyan Atanasov <kaloyanatanasov83@gmail.com>
2023-02-05 20:16:02 +0000

    feat: add projects to projects page.

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,5 +1,55 @@
+import { projects } from '../data';
+import SingleProject from '../components/HomepageProjects/SingleProject';
+import { Grid, Heading } from '../components/ui';
+import meowPortal from '../public/meowPortal.png';
+import adrastos from '../public/adrastos.png';
+import tintsAndShades from '../public/tintsAndShades.png';
+
+// Not final. 
 const Projects = () => {
-  return <div>Projects Page</div>;
+  const [firstProject, secondProject, thirdProject] = projects;
+  return (
+    <>
+      <Grid
+        gap="small"
+        minHeight="100%"
+        padding="smallLeftAndRight"
+        columnsMediaBreakpoint={600}
+        placeContent="center"
+        overflow="hiddenX"
+      >
+        <Heading
+          as="h2"
+          accentType="dot"
+          accentText="."
+          headingText="Projects"
+          title="Projects"
+        />
+        {/* TODO add paragraph here  */}
+      </Grid>
+      <SingleProject
+        name={firstProject.name}
+        description={firstProject.description}
+        stack={firstProject.stack}
+        image={meowPortal}
+        panelColor="blue"
+      />
+      <SingleProject
+        name={secondProject.name}
+        description={secondProject.description}
+        stack={secondProject.stack}
+        image={adrastos}
+        panelColor="red"
+      />
+      <SingleProject
+        name={thirdProject.name}
+        description={thirdProject.description}
+        stack={thirdProject.stack}
+        image={tintsAndShades}
+        panelColor="darkGrey"
+      />
+    </>
+  );
 };
 
 export default Projects;
