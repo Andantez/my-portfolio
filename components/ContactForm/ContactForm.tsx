d1bb5c67749621dd0e0e5117bb2e0cc5cd3c70fb
Kaloyan Atanasov <kaloyanatanasov83@gmail.com>
2023-02-15 20:55:33 +0000

    feat: add notification component.

--- components/ContactForm/ContactForm.tsx
+++ components/ContactForm/ContactForm.tsx
@@ -6,7 +6,7 @@ import type { FormInputs, Error } from '../../lib/types/form';
 import InputWrapper from './InputWrapper';
 import FormControl from './FormControl';
 import { Button, ProgressBar } from '../ui';
-
+import Notification from '../Notification';
 const ContactForm = (): JSX.Element => {
   const {
     register,
@@ -23,7 +23,7 @@ const ContactForm = (): JSX.Element => {
       message: '',
     },
   });
-  const [showNotification, setShowNotification] = useState(false);
+  const [showNotification, setShowNotification] = useState(true);
   const messageLength = watch('message').length;
 
   const onSubmit: SubmitHandler<FormInputs> = async (data) => {
@@ -58,102 +58,106 @@ const ContactForm = (): JSX.Element => {
       reset();
     }
   }, [isSubmitSuccessful, reset]);
-  return (
-    <form className={formContainer} onSubmit={handleSubmit(onSubmit)}>
-      <div className={wrapper}>
-        <InputWrapper>
-          <FormControl
-            as="input"
-            name="name"
-            label="Name"
-            register={register}
-            errors={errors}
-            required={true}
-            validationSchema={{
-              required: 'Please supply a valid name',
-              minLength: {
-                value: 2,
-                message: 'Please supply a valid name',
-              },
-            }}
-          />
-        </InputWrapper>
-        <span className={formError}>{errors?.name?.message}</span>
-      </div>
-      <div className={wrapper}>
-        <InputWrapper>
-          <FormControl
-            as="input"
-            label="Email"
-            type="email"
-            name="email"
-            register={register}
-            errors={errors}
-            required={true}
-            validationSchema={{
-              required: 'Please enter an email address',
-              pattern: {
-                value: REGEX,
-                message:
-                  'Enter a valid email address e.g in the format user@domain.com',
-              },
-            }}
-          />
-        </InputWrapper>
-        <span className={formError}>{errors?.email?.message}</span>
-      </div>
-      <div className={wrapper}>
-        <InputWrapper>
-          <FormControl
-            as="input"
-            name="subject"
-            label="Subject"
-            type="text"
-            register={register}
-            errors={errors}
-            required={false}
-            validationSchema={{
-              maxLength: {
-                value: 180,
-                message: 'The Subject may not be longer than 180 characters',
-              },
-            }}
-          />
-        </InputWrapper>
-        <span className={formError}>{errors?.subject?.message}</span>
-      </div>
-      <div className={wrapper}>
-        <InputWrapper>
-          <FormControl
-            as="textarea"
-            name="message"
-            label="Message"
-            register={register}
-            errors={errors}
-            required={true}
-            validationSchema={{
-              required: "Don't be shy! Write something",
-              maxLength: MAX_MESSAGE_LENGTH,
-            }}
-            cols={30}
-            rows={5}
-          />
-        </InputWrapper>
-        <div>
-          {errors?.message?.message ? (
-            <span className={formError}>{errors.message.message}</span>
-          ) : (
-            <ProgressBar messageLength={messageLength} />
-          )}
+  if (showNotification) {
+    return <Notification />
+  } else {
+    return (
+      <form className={formContainer} onSubmit={handleSubmit(onSubmit)}>
+        <div className={wrapper}>
+          <InputWrapper>
+            <FormControl
+              as="input"
+              name="name"
+              label="Name"
+              register={register}
+              errors={errors}
+              required={true}
+              validationSchema={{
+                required: 'Please supply a valid name',
+                minLength: {
+                  value: 2,
+                  message: 'Please supply a valid name',
+                },
+              }}
+            />
+          </InputWrapper>
+          <span className={formError}>{errors?.name?.message}</span>
         </div>
-      </div>
-      <Button type="submit">Send your message</Button>
-      {/* next line is temporary */}
-      {showNotification && (
-        <h3>Thank You. I will get back at you as soon as possible!</h3>
-      )}
-    </form>
-  );
+        <div className={wrapper}>
+          <InputWrapper>
+            <FormControl
+              as="input"
+              label="Email"
+              type="email"
+              name="email"
+              register={register}
+              errors={errors}
+              required={true}
+              validationSchema={{
+                required: 'Please enter an email address',
+                pattern: {
+                  value: REGEX,
+                  message:
+                    'Enter a valid email address e.g in the format user@domain.com',
+                },
+              }}
+            />
+          </InputWrapper>
+          <span className={formError}>{errors?.email?.message}</span>
+        </div>
+        <div className={wrapper}>
+          <InputWrapper>
+            <FormControl
+              as="input"
+              name="subject"
+              label="Subject"
+              type="text"
+              register={register}
+              errors={errors}
+              required={false}
+              validationSchema={{
+                maxLength: {
+                  value: 180,
+                  message: 'The Subject may not be longer than 180 characters',
+                },
+              }}
+            />
+          </InputWrapper>
+          <span className={formError}>{errors?.subject?.message}</span>
+        </div>
+        <div className={wrapper}>
+          <InputWrapper>
+            <FormControl
+              as="textarea"
+              name="message"
+              label="Message"
+              register={register}
+              errors={errors}
+              required={true}
+              validationSchema={{
+                required: "Don't be shy! Write something",
+                maxLength: MAX_MESSAGE_LENGTH,
+              }}
+              cols={30}
+              rows={5}
+            />
+          </InputWrapper>
+          <div>
+            {errors?.message?.message ? (
+              <span className={formError}>{errors.message.message}</span>
+            ) : (
+              <ProgressBar messageLength={messageLength} />
+            )}
+          </div>
+        </div>
+        <Button type="submit">Send your message</Button>
+        {/* next line is temporary */}
+        {showNotification && (
+          <h3>Thank You. I will get back at you as soon as possible!</h3>
+        )}
+      </form>
+    );
+  }
 };
 
 export default ContactForm;
---
+++ components/Notification/Notification.tsx
@@ -0,0 +1,5 @@
+const Notification = () => {
+  return <div>Success</div>;
+};
+
+export default Notification;
