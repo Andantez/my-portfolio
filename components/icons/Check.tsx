592b59267a54316256bf769b1c83206c9a2720eb
Kaloyan Atanasov <kaloyanatanasov83@gmail.com>
2023-02-19 15:39:05 +0000

    change: check svg type.

--- components/icons/Check.tsx
+++ components/icons/Check.tsx
@@ -1,12 +1,13 @@
+import {  motion, SVGMotionProps } from 'framer-motion';
 import { iconShadow } from '../../styles/SharedStyles.css';
 
 const Check = ({
   width = '5rem',
   height = '5rem',
   ...rest
-}: React.SVGProps<SVGSVGElement>): JSX.Element => {
+}: SVGMotionProps<SVGSVGElement>): JSX.Element => {
   return (
-    <svg
+    <motion.svg
       className={iconShadow}
       viewBox="0 0 512 512"
       fill="#A1CC4D"
@@ -15,7 +16,7 @@ const Check = ({
       {...rest}
     >
       <path d="M470.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L192 338.7l233.4-233.3c12.5-12.5 32.8-12.5 45.3 0z" />
-    </svg>
+    </motion.svg>
   );
 };
 
