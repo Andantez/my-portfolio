6fa039a76e90fcd2b667eb1250891723cfd9ea4a
Kaloyan Atanasov <kaloyanatanasov83@gmail.com>
2023-03-06 22:46:38 +0000

    feat: animate theme toggle icons: NOT FINAL.

--- components/icons/Moon.tsx
+++ components/icons/Moon.tsx
@@ -1,9 +1,40 @@
 import { textColor } from '../../styles/vars.css';
+import { motion, Variants } from 'framer-motion';
 
+const moon: Variants = {
+  initial: {
+    pathLength: 0,
+    rotate: 90,
+  },
+  animate: {
+    pathLength: 1,
+    rotate: 0,
+    transition: {
+      type: 'spring',
+      stiffness: 75,
+    },
+  },
+  exit: {
+    pathLength: 0,
+    rotate: 90,
+    transition: {
+      duration: 0.5,
+    },
+  },
+};
 const Moon = (): JSX.Element => {
   return (
-    <svg viewBox="0 0 512 512" fill="currentColor" height="25px" width="25px">
-      <path
+    <motion.svg
+      viewBox="0 0 512 512"
+      fill="currentColor"
+      height="25px"
+      width="25px"
+      initial="initial"
+      animate="animate"
+      exit="exit"
+    >
+      <motion.path
+        variants={moon}
         fill="none"
         stroke={textColor}
         strokeLinecap="round"
@@ -11,7 +42,7 @@ const Moon = (): JSX.Element => {
         strokeWidth={32}
         d="M160 136c0-30.62 4.51-61.61 16-88C99.57 81.27 48 159.32 48 248c0 119.29 96.71 216 216 216 88.68 0 166.73-51.57 200-128-26.39 11.49-57.38 16-88 16-119.29 0-216-96.71-216-216z"
       />
-    </svg>
+    </motion.svg>
   );
 };
 
--- components/icons/Sun.tsx
+++ components/icons/Sun.tsx
@@ -1,8 +1,52 @@
 import { textColor } from '../../styles/vars.css';
+import { motion, Variants } from 'framer-motion';
 
+const sun: Variants = {
+  initial: {
+    pathLength: 0,
+    // opacity: 0,
+    // rotate: 90,
+  },
+  animate: {
+    pathLength: 1,
+    // opacity: 1,
+    rotate: 0,
+    transition: {
+      duration: 0.5,
+    },
+  },
+  exit: {
+    pathLength: 0,
+    // opacity: 0,
+    // rotate: 90,
+    transition: {
+      duration: 0.5,
+    },
+  },
+};
+const sun2: Variants = {
+  initial: {
+    pathLength: 0,
+    opacity: 0,
+  },
+  animate: {
+    pathLength: 1,
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+    },
+  },
+  exit: {
+    pathLength: 0,
+    opacity: 0,
+    transition: {
+      duration: 0.5,
+    },
+  },
+};
 const Sun = (): JSX.Element => {
   return (
-    <svg
+    <motion.svg
       fill="none"
       stroke={textColor}
       strokeLinecap="round"
@@ -11,10 +55,19 @@ const Sun = (): JSX.Element => {
       viewBox="0 0 24 24"
       height="25px"
       width="25px"
+      initial="initial"
+      animate="animate"
+      exit="exit"
     >
-      <path d="M17 12 A5 5 0 0 1 12 17 A5 5 0 0 1 7 12 A5 5 0 0 1 17 12 z" />
-      <path d="M12 1v2M12 21v2M4.22 4.22l1.42 1.42M18.36 18.36l1.42 1.42M1 12h2M21 12h2M4.22 19.78l1.42-1.42M18.36 5.64l1.42-1.42" />
-    </svg>
+      <motion.path
+        d="M17 12 A5 5 0 0 1 12 17 A5 5 0 0 1 7 12 A5 5 0 0 1 17 12 z"
+        variants={sun}
+      />
+      <motion.path
+        variants={sun2}
+        d="M12 1v2M12 21v2M4.22 4.22l1.42 1.42M18.36 18.36l1.42 1.42M1 12h2M21 12h2M4.22 19.78l1.42-1.42M18.36 5.64l1.42-1.42"
+      />
+    </motion.svg>
   );
 };
 
