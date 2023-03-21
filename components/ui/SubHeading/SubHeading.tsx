4f62d94b63e2e8647cbaa483717f74ddd8d99086
Kaloyan Atanasov <kaloyanatanasov83@gmail.com>
2023-03-21 21:12:59 +0000

    feat: make the component polymorphic.

--- components/ui/SubHeading/SubHeading.tsx
+++ components/ui/SubHeading/SubHeading.tsx
@@ -1,5 +1,20 @@
-const SubHeading = () => {
-  return <div>subHeading</div>;
+type Heading = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
+
+type SubHeadingOwnProps<T extends React.ElementType> = {
+  children: React.ReactNode;
+  as?: T;
+};
+
+type SubHeadingProps<T extends React.ElementType> = SubHeadingOwnProps<T> &
+  Omit<React.ComponentPropsWithoutRef<T>, keyof SubHeadingOwnProps<T>>;
+
+const SubHeading = <T extends React.ElementType = Heading>({
+  children,
+  as,
+}: SubHeadingProps<T>): JSX.Element => {
+  const ComponentHeading = as || 'h1';
+
+  return <ComponentHeading>{children}</ComponentHeading>;
 };
 
 export default SubHeading;
