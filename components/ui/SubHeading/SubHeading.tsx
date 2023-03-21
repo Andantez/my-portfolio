c2a42e57ecead253f59bbd4bc527ffc2bd25f120
Kaloyan Atanasov <kaloyanatanasov83@gmail.com>
2023-03-21 21:30:35 +0000

    feat: move Heading and Styling types to separate file.

--- components/ui/SubHeading/SubHeading.tsx
+++ components/ui/SubHeading/SubHeading.tsx
@@ -1,8 +1,9 @@
-type Heading = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
+import type { Heading, Styling } from '../../../lib/types/heading';
 
 type SubHeadingOwnProps<T extends React.ElementType> = {
   children: React.ReactNode;
   as?: T;
+  cssStyling: Styling;
 };
 
 type SubHeadingProps<T extends React.ElementType> = SubHeadingOwnProps<T> &
@@ -11,10 +12,11 @@ type SubHeadingProps<T extends React.ElementType> = SubHeadingOwnProps<T> &
 const SubHeading = <T extends React.ElementType = Heading>({
   children,
   as,
+  cssStyling,
 }: SubHeadingProps<T>): JSX.Element => {
   const ComponentHeading = as || 'h1';
 
-  return <ComponentHeading>{children}</ComponentHeading>;
+  return <ComponentHeading className={cssStyling}>{children}</ComponentHeading>;
 };
 
 export default SubHeading;
