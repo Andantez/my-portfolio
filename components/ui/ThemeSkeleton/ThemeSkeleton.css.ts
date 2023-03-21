46105f5d0ee219207acf2e158370fd2fc11fbacf
Kaloyan Atanasov <kaloyanatanasov83@gmail.com>
2023-03-21 19:41:29 +0000

    feat: render placeholder till Theme toggle button is loaded.

---
+++ components/ui/ThemeSkeleton/ThemeSkeleton.css.ts
@@ -0,0 +1,7 @@
+import { style } from '@vanilla-extract/css';
+
+export const skeleton = style({
+  display: 'inline-block',
+  width: 20,
+  height: 20,
+});
---
+++ components/ui/ThemeSkeleton/ThemeSkeleton.tsx
@@ -0,0 +1,7 @@
+import { skeleton } from './ThemeSkeleton.css';
+
+const ThemeSkeleton = () => {
+  return <span className={skeleton}></span>;
+};
+
+export default ThemeSkeleton;
