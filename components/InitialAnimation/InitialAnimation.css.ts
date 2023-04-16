49e109e69dcd8d053c2603df365b1aa990e19e5a
Kaloyan Atanasov <kaloyanatanasov83@gmail.com>
2023-04-16 22:43:24 +0100

    feat: add loading animation.

--- components/InitialAnimation/InitialAnimation.css.ts
+++ components/InitialAnimation/InitialAnimation.css.ts
@@ -1,8 +1,18 @@
 import { style } from '@vanilla-extract/css';
+import { modalBG, modalTextColor } from '../../styles/vars.css';
 
 export const animationContainer = style({
   position: 'fixed',
   inset: 0,
-  background: 'red',
+  background: modalBG,
   zIndex: 1,
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+});
+
+export const heading = style({
+  color: modalTextColor,
+  fontFamily: 'var(--ff-heading)',
+  fontSize: '13rem',
 });
--- components/InitialAnimation/InitialAnimation.tsx
+++ components/InitialAnimation/InitialAnimation.tsx
@@ -1,14 +1,64 @@
-import { motion } from 'framer-motion';
-import { animationContainer } from './InitialAnimation.css';
+import { motion, Variants } from 'framer-motion';
+import { animationContainer, heading } from './InitialAnimation.css';
+import { accent } from '../../styles/SharedStyles.css';
 
+const textVariant: Variants = {
+  hidden: {
+    WebkitMaskImage: `repeating-linear-gradient(to left, rgba(0,0,0,0) 0px, rgba(0,0,0,0) 25px, rgba(0,0,0,1) 25px, rgba(0,0,0,1) 25px)`,
+    opacity: 0,
+  },
+  visible: {
+    WebkitMaskImage: `repeating-linear-gradient(to left, rgba(0,0,0,0) 0px, rgba(0,0,0,0) 0px, rgba(0,0,0,1) 0px, rgba(0,0,0,1) 25px)`,
+    opacity: 1,
+    transition: {
+      duration: 1,
+      ease: 'easeInOut',
+    },
+  },
+};
+
+const dotVariant: Variants = {
+  initial: {
+    opacity: 0,
+    y: '-100%',
+  },
+  animate: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      delay: 1,
+      type: 'spring',
+      stiffness: 225,
+      damping: 7.5,
+    },
+  },
+};
 const InitialAnimation = () => {
   return (
     <motion.div
-      initial={{ clipPath: 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)' }}
-      animate={{ clipPath: 'polygon(0% 100%, 100% 100%, 100% 100%, 0% 100%)' }}
+      // initial={{ clipPath: 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)' }}
+      // animate={{ clipPath: 'polygon(0% 100%, 100% 100%, 100% 100%, 0% 100%)' }}
       transition={{ duration: 1.6 }}
       className={animationContainer}
-    />
+    >
+      <motion.h1
+        className={heading}
+        initial="hidden"
+        animate="visible"
+        variants={textVariant}
+      >
+        Loading
+        <motion.span
+          style={{ display: 'inline-block' }}
+          initial="initial"
+          animate="animate"
+          variants={dotVariant}
+          className={accent.base}
+        >
+          .
+        </motion.span>
+      </motion.h1>
+    </motion.div>
   );
 };
 
