5698e1d87bb1baf2a4c9cd582a148258a99c9422
Kaloyan Atanasov <kaloyanatanasov83@gmail.com>
2023-07-05 22:51:08 +0100

    add: styling to project container and media query.

--- components/HomepageProjects/HomepageProjects.css.ts
+++ components/HomepageProjects/HomepageProjects.css.ts
@@ -7,12 +7,23 @@ export const homepageProjects = style({
   gap: gap500,
 });
 
-// TEMPORARY
 export const homepageProjectsContainer = style({
-  border: '1px solid #363636',
+  display: 'flex',
+  flexDirection: 'column',
+  boxShadow: '0 0 25px 3px rgba(175,175,175,.5)',
   maxWidth: '1400px',
-  margin: '0 auto',
-  padding: '5rem',
-  backgroundColor: '#363636',
-  marginTop: '5rem',
+  margin: '5rem 1rem 0',
+  padding: '0 1rem',
+  borderRadius: '.5rem',
+  '@media': {
+    'screen and (min-width: 46.875rem)': {
+      padding: 0,
+      margin: '5rem 2.5rem 0',
+    },
+    'screen and (min-width: 87.5rem)': {
+      margin: '7rem auto 0',
+      padding: '7rem 5rem',
+      gap: '7.5rem',
+    },
+  },
 });
