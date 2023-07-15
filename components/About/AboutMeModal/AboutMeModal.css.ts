b70a9320c83bfb2b22a2a4a570549dd226ff0e8f
Kaloyan Atanasov <kaloyanatanasov83@gmail.com>
2023-07-15 22:46:53 +0100

    feat: add dot and line next to the sub headings.

--- components/About/AboutMeModal/AboutMeModal.css.ts
+++ components/About/AboutMeModal/AboutMeModal.css.ts
@@ -1,5 +1,6 @@
 import { style } from '@vanilla-extract/css';
 import {
+  accentColor,
   backgroundColor,
   fs400,
   fs450,
@@ -8,6 +9,7 @@ import {
   gap150,
   gap200,
   textColor,
+  titleTextColor,
 } from '../../../styles/vars.css';
 
 export const modalContainer = style({
@@ -26,9 +28,20 @@ export const contentContainer = style({
 });
 
 export const articlesWrapper = style({
+  position: 'relative',
   display: 'grid',
   gap: gap150,
   fontFamily: 'var(--ff-text)',
+  paddingBottom: '.5rem',
+  '::before': {
+    content: '',
+    position: 'absolute',
+    left: 0,
+    top: 0,
+    width: '2px',
+    height: '100%',
+    background: titleTextColor,
+  },
 });
 
 export const mainHeading = style({
@@ -53,9 +66,22 @@ export const skillsUL = style({
 });
 
 export const articleWrapper = style({
+  position: 'relative',
   display: 'grid',
   gap: '0.75rem',
   color: textColor,
+  paddingLeft: '1.5rem',
+  '::before': {
+    content: '',
+    position: 'absolute',
+    top: 0,
+    left: '-6px',
+    width: 14,
+    height: 14,
+    background: backgroundColor,
+    borderRadius: '50%',
+    border: `2px solid ${accentColor}`,
+  },
 });
 
 export const articleItem = style({
