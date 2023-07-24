c1e84bc118508d0c616948124454548b1286d650
Kaloyan Atanasov <kaloyanatanasov83@gmail.com>
2023-07-24 22:49:04 +0100

    feat: disable scroll hook adds padding right to body when modal is open to prevent content shift.

--- lib/hooks/useDisableScroll.tsx
+++ lib/hooks/useDisableScroll.tsx
@@ -2,9 +2,17 @@ import { useEffect } from 'react';
 
 const useDisableScroll = (isOpen: boolean) => {
   useEffect(() => {
+    const scrollbarWidth =
+      window.innerWidth - document.documentElement.clientWidth;
+    if (isOpen && scrollbarWidth > 0) {
+      document.body.style.paddingRight = `${scrollbarWidth}px`;
+    }
+
     document.body.style.overflow = isOpen ? 'hidden' : '';
+
     return () => {
       document.body.style.overflow = '';
+      document.body.style.paddingRight = '';
     };
   }, [isOpen]);
 };
